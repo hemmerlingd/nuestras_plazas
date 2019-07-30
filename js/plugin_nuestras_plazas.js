6bbe2546b9ae8eccbaf968b763f096bf6fc7a595
ignacio perlo <perloignacio@gmail.com>
2019-07-30 09:26:57 -0300

    Auto stash before merge of "master" and "nuestras_plazas/master"

--- js/plugin_nuestras_plazas.js
+++ js/plugin_nuestras_plazas.js
@@ -35,6 +35,7 @@ function mapaPlazas()
 	map = new google.maps.Map(document.getElementById('map'), {
 		  center: { lat: -31.4228241, lng: -64.1688174 },
           zoom: 12,
+
           mapTypeId: google.maps.MapTypeId.ROADMAP
       });
 
@@ -46,10 +47,18 @@ function mapaPlazas()
 function marcaespacio(lista,nombre,contratista,color)
 {
 	
+<<<<<<< Updated upstream
 	//console.log(lista);
 	var arr = new Array();
 	var poligono =[];
 	for (var i = lista.length - 1; i >= 0; i--) {
+=======
+	//var poligono = [];
+	
+	var poligono =[];
+	console.log(lista);
+	for (i=0;i<=lista.length-1;i++) {
+>>>>>>> Stashed changes
 		/*---------COLOR-----------*/
 		var a = Math.floor((Math.random() * 255) + 1);
 		var b = Math.floor((Math.random() * 255) + 1);
@@ -57,22 +66,44 @@ function marcaespacio(lista,nombre,contratista,color)
 		var color = 'rgb('+a+','+b+','+c+')';
 		/*-------FIN COLOR---------*/
 		
+<<<<<<< Updated upstream
 		var plantilla="<div class='info'><div class='cabecera'><div class='icono'><img src='https://www.cordoba.gob.ar/wp-content/uploads/2019/07/arbol.png'></div><div class='titulo'>NUESTRAS PLAZAS Y PASESO</div></div><hr><div class='nombre'>PLAZA COLÓN</div><div class='foto'><img src='https://gobiernoabierto.cordoba.gob.ar/media/__sized__/imagenes/obras/IMG_7346-thumbnail-500x500-70.JPG'></div><div class='contratista'>JUAN PEREZ</div><a href=''><span>Ver más info</span></a></div>";
 		
 		
 		for (var a = lista[i].obj.length - 1; a >= 0; a--) {
+=======
+		
+		var plantilla="<div class='info'><div class='cabecera'><div class='icono'><img src='https://www.cordoba.gob.ar/wp-content/uploads/2019/07/arbol.png'></div><div class='titulo'>NUESTRAS PLAZAS Y PASESO</div></div><hr><div class='nombre'>PLAZA COLÓN</div><div class='foto'><img src='https://gobiernoabierto.cordoba.gob.ar/media/__sized__/imagenes/obras/IMG_7346-thumbnail-500x500-70.JPG'></div><div class='contratista'>JUAN PEREZ</div><a href=''><span>Ver más info</span></a></div>";
+		var arr = new Array();
+		//if(i==0){ar=0;}else{ar=lista[i-1].obj.length+1;}
+		for (a=0;a<=lista[i].obj.length-1;a++) {
+>>>>>>> Stashed changes
 			arr.push( new google.maps.LatLng(
 	                    parseFloat(lista[i].obj[a].lat),
 	                    parseFloat(lista[i].obj[a].lng)
 	            ));
 		}
+<<<<<<< Updated upstream
 		poligono.push(new google.maps.Polygon({
             paths: arr,
+=======
+		arr.push(new google.maps.LatLng(
+                    parseFloat(lista[i].obj[0].lat),
+                    parseFloat(lista[i].obj[0].lng)
+		));
+
+		console.log(arr);
+
+		poli=new google.maps.Polyline({
+            path: arr,
+>>>>>>> Stashed changes
             strokeColor: color,
             strokeOpacity: 0.8,
+            fillColor:color,
             strokeWeight: 2,
-            fillColor: color,
+            geodesic: true,
             fillOpacity: 0.35
+<<<<<<< Updated upstream
         }));
 		//console.log(lista[i].obj);
 		poligono[poligono.length-1].setMap(map);
@@ -87,6 +118,24 @@ function marcaespacio(lista,nombre,contratista,color)
 
 		arr=[];
 		poligono=[];
+=======
+   		})
+
+		//poligono.push();
+		poli.setMap(map);
+		
+
+		
+	}
+	
+	//dibuja(poligono);
+}
+
+function dibuja(poligonos)
+{
+	for (var i = poligonos.length - 1; i >= 0; i--) {
+		
+>>>>>>> Stashed changes
 	}
 		
 }	
