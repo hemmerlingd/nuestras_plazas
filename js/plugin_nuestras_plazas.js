75c3f0f426e462c5cbf90c69eabf5d74c11176f0
Unknown <hemmerling@gmail.com>
2019-07-30 09:22:58 -0300

    plazas

--- js/plugin_nuestras_plazas.js
+++ js/plugin_nuestras_plazas.js
@@ -1,3 +1,32 @@
+(function(window, document, $) {
+const $plazas = $('#plazas');
+const $form = $plazas.find('form');
+let $resultados = $plazas.find('.resultados');
+
+$form.submit(function(e) {
+    e.preventDefault();
+    const datos = $form.serializeArray();
+    //console.log(datos);
+    $.ajax({
+      type: "POST",
+      dataType: "JSON",
+      url: nuestras_plazas.url,
+      data: {
+        action: 'nuestras_plazas',
+        nonce: nuestras_plazas.nonce,
+        zona: datos[0].value
+      },
+      success: function(response) {
+        if (response.data) {
+        	//console.log(response.data);
+          $resultados.html(response.data);
+        }
+      }
+    });
+  });
+
+})(window, document, jQuery);
+
 var map;
 
 
@@ -17,10 +46,9 @@ function mapaPlazas()
 function marcaespacio(lista,nombre,contratista,color)
 {
 	
-	//var poligono = [];
+	//console.log(lista);
 	var arr = new Array();
 	var poligono =[];
-
 	for (var i = lista.length - 1; i >= 0; i--) {
 		/*---------COLOR-----------*/
 		var a = Math.floor((Math.random() * 255) + 1);
@@ -29,9 +57,8 @@ function marcaespacio(lista,nombre,contratista,color)
 		var color = 'rgb('+a+','+b+','+c+')';
 		/*-------FIN COLOR---------*/
 		
-		arr=[];
-		poligono=[];
 		var plantilla="<div class='info'><div class='cabecera'><div class='icono'><img src='https://www.cordoba.gob.ar/wp-content/uploads/2019/07/arbol.png'></div><div class='titulo'>NUESTRAS PLAZAS Y PASESO</div></div><hr><div class='nombre'>PLAZA COLÓN</div><div class='foto'><img src='https://gobiernoabierto.cordoba.gob.ar/media/__sized__/imagenes/obras/IMG_7346-thumbnail-500x500-70.JPG'></div><div class='contratista'>JUAN PEREZ</div><a href=''><span>Ver más info</span></a></div>";
+		
 		
 		for (var a = lista[i].obj.length - 1; a >= 0; a--) {
 			arr.push( new google.maps.LatLng(
@@ -39,9 +66,6 @@ function marcaespacio(lista,nombre,contratista,color)
 	                    parseFloat(lista[i].obj[a].lng)
 	            ));
 		}
-
-		console.log(arr);
-
 		poligono.push(new google.maps.Polygon({
             paths: arr,
             strokeColor: color,
@@ -50,21 +74,24 @@ function marcaespacio(lista,nombre,contratista,color)
             fillColor: color,
             fillOpacity: 0.35
         }));
-
+		//console.log(lista[i].obj);
 		poligono[poligono.length-1].setMap(map);
-		//console.log(poligono);
-	   // var popup = new google.maps.InfoWindow();
 
+	    var popup = new google.maps.InfoWindow();
 	   /* poligono.addListener('click', function (e) {
 		    popup.setContent(plantilla);
 		    popup.setPosition(e.latLng);
 		    popup.open(map);
 		});*/
-	}
-}	
-function filtra()
-{
-	//marcaespacio(verticesPoligono,"Plaza Colón","Juan Perez","rgb(0,0,0)");
-}
 
+
+		arr=[];
+		poligono=[];
+	}
+		
+}	
+function filtra(lista)
+{
+	marcaespacio(lista,"Plaza Colón","Juan Perez","rgb(0,0,0)");
+}
 
--- nuestras_plazas.php
+++ nuestras_plazas.php
@@ -34,101 +34,81 @@ class nuestras_plazas
 
 {
 
+	private static $URL_API_GOB = 'https://gobiernoabierto.cordoba.gob.ar/api/v2/espacios-verdes/frentes-espacios-verdes/';
+	public $nonce_plazas = '';
 	public static $instancia = null;
 
-
-
 	public static function get_instancia() {
-
 		if (null == self::$instancia) {
-
 			self::$instancia = new nuestras_plazas();
-
 		} 
-
 		return self::$instancia;
-
 	}
-
-	
-
-	
 
 	private function __construct()	
-
 	{
-
-		
-
-		
-
 		add_shortcode('nuestras_plazas', array($this, 'render_shortcode_nuestras_plazas'));
-
 		add_action('wp_enqueue_scripts', array($this, 'cargar_assets'));
-
+		add_action('wp_ajax_nuestras_plazas', array($this, 'nuestras_plazas')); 
+		add_action('wp_ajax_nopriv_nuestras_plazas', array($this, 'nuestras_plazas'));
 		//add_action('init', array($this, 'boton_shortcode_repecticiones'));
-
 	}
-
-	
 
 	public function cargar_assets()
 
 	{
 
 		$urlCSSShortcode = $this->cargar_url_asset('/css/shortcode_nuestras_plazas.css');
-
 		 wp_register_style('plugin_nuestras_plazas_css', $urlCSSShortcode);
-
-		 wp_enqueue_style('plugin_nuestras_plazas_css', $urlCSSShortcode);
-
-		 
-
-		 $urlJSNuestrasPlazas = $this->cargar_url_asset('/js/plugin_nuestras_plazas.js');
-
-		 
-
+		$urlJSNuestrasPlazas = $this->cargar_url_asset('/js/plugin_nuestras_plazas.js');
 		 wp_register_script('carga-plugin-nuestras_plazas', $urlJSNuestrasPlazas,null,false,false);
+		
+		global $post;
+	    if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'nuestras_plazas') ) {
+			wp_enqueue_script(
+				'nuestras_plazas_ajax', 
+				$urlJSNuestrasPlazas, 
+				array('jquery'), 
+				'1.0.0',
+				TRUE
+			);
+			//wp_enqueue_style('buscador_quinteros.css', $this->cargar_url_asset('/css/shortcodeQuinteros.css'));
+			$nonce_plazas = wp_create_nonce("nuestras_plazas_nonce");
+			wp_localize_script(
+				'nuestras_plazas_ajax', 
+				'nuestras_plazas', 
+				array(
+					'url'   => admin_url('admin-ajax.php'),
+					'nonce' => $nonce_plazas
+				)
+			);
 
-
+		}
 
 	}
 
 	private function cargar_url_asset($ruta_archivo)
 
 	{
-
 		return plugins_url($ruta_archivo, __FILE__);
-
 	}
-
-	
 
 	public function chequear_respuesta($api_response, $tipoObjeto, $nombre_transient)
 
 	{
 
 		if (is_null($api_response)) {
-
 			return [ 'results' => [] ];
-
 		} else if (is_wp_error($api_response)) {
-
 			return [ 'results' => [], 'error' => 'Ocurri&oacute; un error al cargar '.$tipoObjeto.'.'.$mensaje];
-
 		} else {
-
 			$respuesta = json_decode(wp_remote_retrieve_body($api_response), true);
-
 			return $respuesta;
-
 		}
-
 	}
 
 	public function cargalistas($url,$lista_zonas,$lista_contratistas)
 	{
-		
 		$data = file_get_contents($url);
 		$resultado = json_decode($data);
 		foreach ($resultado->results as $zona) {
@@ -141,7 +121,6 @@ class nuestras_plazas
 		$listas=["zonas"=>$lista_zonas,"contratistas"=>$lista_contratistas];
 		if(!empty($resultado->next))
 		{
-
 			return $this->cargalistas($resultado->next,$lista_zonas,$lista_contratistas);
 		}else{
 			return $listas;
@@ -160,61 +139,48 @@ class nuestras_plazas
 			//var_dump($resultado->results);
 		foreach ($resultado->results->features as $zona) {
 			//var_dump($zona);
-			$objzona=['id'=> $zona->id, 'nombre' => $zona->properties->obra, 'foto'=>$zona->properties->adjuntos[0]->foto->thumbnail_500, 'coordenadas'=> $zona->geometry->coordinates];
+         // referenciar();
+			$objzona=['id'=> $zona->id, 'nombre' => $zona->properties->obra, 'foto'=>$zona->properties->adjuntos[0]->foto->thumbnail_500, 'coordinates'=> $zona->geometry->coordinates];
 			array_push($plazas,(array)$objzona );
-
 		}
 		//$listas=["zonas"=>$lista_zonas,"contratistas"=>$lista_contratistas];
 		if(!empty($resultado->next))
 		{
-
 			return $this->cargamapa($resultado->next,$plazas);
 		}else{
 			//var_dump($plazas);
 			return $plazas;
 		}
-		
-		
-
 	}
 
+	public function coordenadas($dibuja){
+		$listaobj =array();
+		for ($i=0; $i <= count($dibuja)-1; $i++) { 
+		$vertices = array();
+			foreach ($dibuja[$i]['coordinates'] as $coord) {
+				$listadatos = new stdClass();
+				$listadatos->lng= floatval(str_replace(',', '.', $coord[0]));
+				$listadatos->lat= floatval(str_replace(',', '.', $coord[1]));
+				array_push($vertices,$listadatos);
+			}
+			$obj = new stdClass();
+			$obj->obj=$vertices;
+			array_push($listaobj,$obj);
+		}
+		return $listaobj;
+	}
 
 	public function render_shortcode_nuestras_plazas($atributos = [], $content = null, $tag = '')
-
 	{
-		//wp_enqueue_script('carga-plugin-nuestras_plazas',$urlJSNuestrasPlazas,null,false,false);
+		wp_enqueue_style('plugin_nuestras_plazas_css', $urlCSSShortcode);
+		wp_enqueue_script('carga-plugin-nuestras_plazas',$urlJSNuestrasPlazas,null,false,false);
 		$atributos = array_change_key_case((array)$atributos, CASE_LOWER);
-
 	    $atr=shortcode_atts([],$atributos, $tag);
-
 	    $lista_zonas=array();
 		$lista_contratistas=array();
 		$plazas=array();
 		$datos=$this->cargalistas("https://gobiernoabierto.cordoba.gob.ar/api/v2/espacios-verdes/espacios-verdes/",$lista_zonas,$lista_contratistas);
-		$dibuja =$this->cargamapa("https://gobiernoabierto.cordoba.gob.ar/api/v2/espacios-verdes/frentes-espacios-verdes/?id_zona=8",$plazas);
-			
-			$listaobj =array();
-			$vertices = array();
-//count($dibuja)-1
-			for ($i=0; $i <= 1; $i++) { 
-				foreach ($dibuja[$i]['coordenadas'] as $coord) {
-					$listadatos = new stdClass();
-					$listadatos->lng= floatval(str_replace(',', '.', $coord[0]));
-					$listadatos->lat= floatval(str_replace(',', '.', $coord[1]));
-
-					array_push($vertices,$listadatos);
-
-				}
-				$obj = new stdClass();
-				$obj->obj=$vertices;
-
-
-				array_push($listaobj,$obj);
-
-			}
-			//var_dump($obj);
-
-
+		
 
 		$sc="";
 
@@ -222,75 +188,81 @@ class nuestras_plazas
 		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAdhknpOExGWhcYbEXKLfnPHqND4ejjqpE"
   type="text/javascript"></script>
 		<script src="'. plugin_dir_url( __FILE__ ). 'js/plugin_nuestras_plazas.js"></script>
+		<div id="plazas">
 		<div id="buscador">
-
+			<form>
 				<div class="campo">
-
-					<select id="zonas" onchange="javascript:filtra()">
-
+					<select id="zona" name="zona" onchange="">
 						<option value="">Seleccione su zona</option>';
 						foreach($datos['zonas'] as $zona)
 						{
-
 							$sc.="<option value='".$zona['id']."'>".$zona['nombre']."</option>";
-
 						}
-
-
 			  $sc.='</select>
-
 				</div>
-
 				<div class="campo">
-
 					<select id="tipo" onchange="javascript:filtra()">
-
 						<option value="">Seleccione el contratista</option>';
 						foreach($datos['contratistas'] as $contra)
 						{
-
 							$sc.="<option value='".$contra['CUIT']."'>".$contra['organizacion']."</option>";
-
 						}
-
 			  $sc.='</select>
-
-				</div>
-				
+			  
+			</div>
+			<div class="campo">
+				<button id="filtros__buscar" type="submit">Buscar</button>
+				</div>	</form>
 			</div>';
+				$dibuja =$this->cargamapa("https://gobiernoabierto.cordoba.gob.ar/api/v2/espacios-verdes/frentes-espacios-verdes/?id_zona=2",$plazas);
+				//$coordenadas=$this->coordenadas($dibuja);
+			$sc .= $this->renderizar_resultados($dibuja);
+			return $sc;
+	}
+	
 
-
-			$sc.='<div id="listado-actividades">
-
-				<!--<div id="loading"><img src="'.plugin_dir_url( __FILE__ ).'images/loading.gif" /></div>-->
-
-				<div id="resultados">
-
-				</div>
-
+	public function renderizar_resultados($datos){
+			$coordenadas=$this->coordenadas($datos);	
+			$html.='<!--<div id="loading"><img src="'.plugin_dir_url( __FILE__ ).'images/loading.gif" /></div>-->
+			<div id="resultados">
 			</div>
 			<div id="map"></div>
 			';
-
-
-			
-			
-
-			$sc.='
-	<script>
-	var lista = '.json_encode($listaobj).';
-	mapaPlazas();
-	marcaespacio(lista,"'.$dibuja[1]["coordenadas"].'","Juan Perez","rgb(0,0,0)");</script>
-
-  ';
-
-		return $sc;
+			$html.='
+			<script>
+			var lista = '.json_encode($coordenadas).';
+			mapaPlazas();
+			marcaespacio(lista,"NOMBRE","Juan Perez","rgb(0,0,0)");</script></div>';
+			return $html;
 
 	}
 //Gob. Abierto Key
 //AIzaSyAYw1rqa9mL1-__v8h2CVgsRJmpmP2mP1s
 //Nacho
 	//AIzaSyAdhknpOExGWhcYbEXKLfnPHqND4ejjqpE
+	
+
+	public function nuestras_plazas()
+	{
+		$zona='';
+		if($_REQUEST['zona']){
+			$zona.= $_REQUEST['zona'];
+		}
+		check_ajax_referer('nuestras_plazas_nonce', 'nonce');
+
+		if(true) {
+			$api_response = wp_remote_get(self::$URL_API_GOB.'?id_zona='.$zona, [ 'timeout' => 1000 ]);
+			$api_data = json_decode(wp_remote_retrieve_body($api_response), true);
+			var_dump($api_data);
+			wp_send_json_success($this->renderizar_resultados($api_data));
+		} else {
+			wp_send_json_error($api_data);
+		}
+		die();
+	}
+
+
+
 
 
 	/*public function boton_shortcode_repecticiones() {
